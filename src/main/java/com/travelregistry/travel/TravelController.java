9aa6164b3c2f9d13a9ac4c40fe97e2a7a34fea1f
Gabriel Guesser <gdguesser@gmail.com>
2022-02-19 20:57:50 -0300

    creating a function to calculate distance between 2 sets of latitudes and longitudes

--- src/main/java/com/travelregistry/travel/TravelController.java
+++ src/main/java/com/travelregistry/travel/TravelController.java
@@ -1,8 +1,10 @@
 package com.travelregistry.travel;
 
+import com.travelregistry.utils.DistanceCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,7 +23,20 @@ public class TravelController {
     }
 
     @GetMapping
-    public List<Travel> listAll() {
-        return travelRepository.findAll();
+    public List<TravelDto> listAll() {
+        List<Travel> travelsList = travelRepository.findAll();
+        List<TravelDto> travelsDtoList = new ArrayList<>();
+
+        DistanceCalculator distanceCalculator = new DistanceCalculator();
+
+        System.out.println(distanceCalculator.distance(-26.2697733, -48.8343821, 41.1621416, -8.6394632, "K") + " Kilometers");
+
+        if (!travelsList.isEmpty()) {
+            for (Travel travel : travelsList) {
+                travelsDtoList.add(travel.toDto());
+            }
+        }
+
+        return travelsDtoList;
     }
 }
